aebb8c6290606cf96a496ca7f1c0e1e4d0f64cda
Youssef Abdelnasser <youssef.abdelnasser@ibm.com>
2023-01-04 18:49:05 +0200

    update frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,39 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { PostComponent } from './post/post.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { MatInputModule} from "@angular/material/input";
+import { MatCardModule} from "@angular/material/card";
+import { MatButtonModule} from "@angular/material/button";
+import { MatToolbarModule} from "@angular/material/toolbar";
+import { MatExpansionModule} from "@angular/material/expansion";
+
+
+import { AppComponent } from "./app.component";
+import { PostCreateComponent } from "./posts/post-create/post-create.component";
+import { HeaderComponent } from "./header/header.component";
+import { PostListComponent } from "./posts/post-list/post-list.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostComponent
+    PostCreateComponent,
+    HeaderComponent,
+    PostListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     FormsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatExpansionModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
