7311396426a18b438050a5403d7cb799bad4478b
Youssef Abdelnasser <youssef.abdelnasser@ibm.com>
2023-01-06 20:47:44 +0200

    add routing to client side

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,11 @@ import { MatCardModule} from "@angular/material/card";
 import { MatButtonModule} from "@angular/material/button";
 import { MatToolbarModule} from "@angular/material/toolbar";
 import { MatExpansionModule} from "@angular/material/expansion";
-
-
 import { AppComponent } from "./app.component";
 import { PostCreateComponent } from "./posts/post-create/post-create.component";
 import { HeaderComponent } from "./header/header.component";
 import { PostListComponent } from "./posts/post-list/post-list.component";
+import { AppRoutingModule } from "./app-routing.module";
 
 @NgModule({
   declarations: [
@@ -31,7 +30,8 @@ import { PostListComponent } from "./posts/post-list/post-list.component";
     MatButtonModule,
     MatToolbarModule,
     MatExpansionModule,
-    HttpClientModule
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
