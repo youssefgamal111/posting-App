6b4e449f4caf4cce901a7bd3cf36c85de3ba6157
Youssef Abdelnasser <youssef.abdelnasser@ibm.com>
2023-01-06 20:48:12 +0200

    add routing to client side

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule,Routes } from '@angular/router';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+const routes:Routes=[
+  {path:"create",component:PostCreateComponent},
+  {path:"",component:PostListComponent}
+]
+
+
+
+
+@NgModule({
+  imports: [CommonModule, RouterModule.forRoot(routes),RouterModule,BrowserModule],
+  exports:[RouterModule]
+})
+export class AppRoutingModule { }
