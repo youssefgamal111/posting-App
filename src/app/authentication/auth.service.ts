f305c5ced13b40c1f204bd0b97abd16244a3e4c2
Youssef Abdelnasser <youssef.abdelnasser@ibm.com>
2023-01-11 01:36:12 +0200

    add session timer

--- src/app/authentication/auth.service.ts
+++ src/app/authentication/auth.service.ts
@@ -9,6 +9,8 @@ import { User } from './user.model';
 })
 export class AuthService {
    token:string="";
+   expirein:number=0;
+   tokentimer:any;
    private isAuthenticated=new BehaviorSubject<boolean>(false);
   constructor(private http:HttpClient,private router:Router) { }
   createUser(user:User){
@@ -19,16 +21,22 @@ export class AuthService {
 
   }
   authenticateUser(user:User){
-    this.http.post<{message:string,token:string}>("http://localhost:3000/api/user/login",user).subscribe(
+    this.http.post<{message:string,token:string,expirein:string}>("http://localhost:3000/api/user/login",user).subscribe(
       res=>{
       const token=res.token;
       this.token=token;
+      this.expirein=+res.expirein;
+      this.tokentimer=  setTimeout(() => {
+          this.logOut();
+          alert("session ended");
+        }, this.expirein*1000);
       this.isAuthenticated.next(true);
       this.router.navigate(["/"]);
     }
      )
   }
   logOut(){
+  clearTimeout(this.tokentimer);
     this.token="";
     this.isAuthenticated.next(false);
   }
